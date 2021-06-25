18506cc958469a0279c642ecb9ef59ca6a0b61f6
Nishant Kohli <nishkohli96@gmail.com>
2021-06-26 04:05:01 +0530

    env files add wip

---
+++ src/models/city.model.ts
@@ -0,0 +1,33 @@
+import { getModelForClass, prop } from "@typegoose/typegoose";
+import { Types } from "mongoose";
+
+export enum COUNTRY {
+    INDIA = "INDIA",
+    USA = "USA",
+    CANDA = "CANADA"
+}
+
+export class CitySchema {
+    
+    _id: Types.ObjectId;
+
+    @prop({
+        required: true,
+        minlength: 3,
+    })
+    cityName: string;
+
+    @prop({
+        required: true,
+        enum: COUNTRY
+    })
+    country: COUNTRY;
+}
+
+export const CityModel = getModelForClass(CitySchema, {
+    schemaOptions: {
+        collection: "City",
+        timestamps: true,
+        minimize: true,
+    }
+})
---
+++ src/models/person.model.ts
@@ -0,0 +1,33 @@
+import { getModelForClass, prop, Ref } from "@typegoose/typegoose";
+import { Types } from "mongoose";
+import { CitySchema } from "./city.model";
+
+export class PersonSchema {
+    
+    _id: Types.ObjectId;
+
+    @prop({
+        required: true,
+        minlength: 3,
+    })
+    fullName: string;
+
+    @prop({
+        required: true,
+        minlength: 1,
+    })
+    age: number;
+
+    @prop({
+        required: true,
+    })
+    city: Ref<CitySchema>;
+}
+
+export const PersonModel = getModelForClass(PersonSchema, {
+    schemaOptions: {
+        collection: "Person",
+        timestamps: true,
+        minimize: true,
+    }
+})
