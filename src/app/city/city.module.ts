237eff8454843246c17b25b2d7118cfc83ff45dd
Nishant Kohli <nishkohli96@gmail.com>
2021-07-04 03:07:14 +0530

    done

--- src/app/city/city.module.ts
+++ src/app/city/city.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { CityModel } from '../../models/city.model';
 import { CityController } from './city.controller';
 import { CityService } from './city.service';
-
+import { CityDAL } from './city.dal';
 @Module({
     imports: [],
     controllers: [CityController],
     providers: [CityService,
+        // {
+        //     useClass: CityModel,
+        //     provide: CityModel.name
+        // },
         {
-            useClass: CityModel,
-            provide: CityModel.name
+            useClass: CityDAL,
+            provide: CityDAL.name
         },
     ],
 })
--- src/app/city/city.service.ts
+++ src/app/city/city.service.ts
@@ -2,17 +2,18 @@ import { Injectable, Inject } from '@nestjs/common';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 import { CityModel, CitySchema } from '../../models/city.model';
 import { AddCityDTO, GetCityDTO } from './city.dto';
+import { CityDAL } from './city.dal';
 
 @Injectable()
 export class CityService {
 
     constructor(
-        @Inject(CityModel.name) private cityModel: ModelType<CitySchema>,
+        @Inject("CityDAL") private cityModel: CityDAL ,
     )
     { }
 
     async getCities(): Promise<CitySchema[]> {
-        const res = await this.cityModel.find({});
+        const res = await this.cityModel.findAll({});
         return res;
     }
 
@@ -26,7 +27,7 @@ export class CityService {
     async addCity(body: AddCityDTO): Promise<CitySchema> {
         console.log('resp: ', this.cityModel);
 
-        const res = new this.cityModel(body);
+        const res =  this.cityModel.create(body);
         return res;
     }
 }
